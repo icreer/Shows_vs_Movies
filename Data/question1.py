2b6bcf8f4b19cde56f6468815456c26b6b6905d7
Isaac Creer <isaaccreer@gmail.com>
2022-06-13 17:19:52 -0600

    More updates

--- Data/question1.py
+++ Data/question1.py
@@ -1,6 +1,13 @@
-import pandas
+import pandas as pd
 
 """
 Question 1:
 What is the difference between the average cast size of a move and a tv show?
 """
+
+Number_of_cast = dict()
+
+titles = pd.read_csv("Questions/titles.csv")
+credits = pd.read_csv("Questions/credits.csv")
+print(credits.columns)
+print(titles.columns)
